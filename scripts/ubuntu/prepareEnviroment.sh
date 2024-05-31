b805dae8e7afb9f56722c8d3178be714f117cf74
yongzhuo.ding <yongzhuo.ding@sms-group.com>
2024-05-31 02:25:49 +0000

    add scripts for local apt source

---
+++ scripts/ubuntu/prepareEnviroment.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#Note: current test account is stack. if you use different account (for example xxx) . you must use xxx replace stack in this script
+
+#add local path to apt sources
+sudo sed  -i '1 i\deb [trusted=yes] file:/opt/local/debs ./' /etc/apt/sources.list
+
+#permit and auth
+sudo sed  -i 's/\#\PasswordAuthentication\ no/PasswordAuthentication\ yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+echo "stack ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/stack 
+
+#change root password  format:password:name
+sudo echo -e "stack:stack" | sudo chpasswd 
+
+cd /hom/stack
+sudo -i
+chown -R stack.stack *
+
+#sudo sed  -i '1 i\stack ALL=(ALL) NOPASSWD:ALL' /etc/sudoers.d/stack
+
---
+++ scripts/ubuntu/wifi_off.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+nmcli radio wifi off
+sudo sed  -i 's/\#\ deb\ \[trusted=yes/deb\ \[trusted=yes/g' /etc/apt/sources.list
+
+sudo sed  -i 's/deb\ http:/#deb\ http:/g' /etc/apt/sources.list
+sudo sed  -i 's/\#\#deb\ http:/\#deb\ http:/g' /etc/apt/sources.list
+
+
+#sudo sed  -i 's/\#deb\ \[trusted=yes/deb\ \[trusted=yes/g' /etc/apt/sources.list.d/cloudarchive-zed.list
+sudo sed  -i 's/deb\ \http/\#deb\ \http/g' /etc/apt/sources.list.d/cloudarchive-zed.list
+sudo sed  -i 's/\#\#deb\ \http/\#deb\ \http/g' /etc/apt/sources.list.d/cloudarchive-zed.list
