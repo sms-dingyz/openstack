72eec118fbf65095bbc780c4a2f217ff170bca82
yongzhuo.ding <yongzhuo.ding@sms-group.com>
2024-06-13 06:13:31 +0000

    doc

--- scripts/ubuntu/wifi_off.sh
+++ scripts/ubuntu/wifi_off.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 nmcli radio wifi off
+
+#install mysql
+sudo sed  -i 's/wget\ http:/#wget\ http:/g' /home/stack/openstack/scripts/ubuntu/3_controller_install_mysql.sh
+sudo sed  -i 's/\#\#wget\ http:/#wget\ http:/g' /home/stack/openstack/scripts/ubuntu/3_controller_install_mysql.sh
+
 sudo sed  -i 's/\#\ deb\ \[trusted=yes/deb\ \[trusted=yes/g' /etc/apt/sources.list
 
 sudo sed  -i 's/deb\ http:/#deb\ http:/g' /etc/apt/sources.list
